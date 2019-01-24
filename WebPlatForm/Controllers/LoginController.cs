cba8ca3cdb896798ed438fe0f432b0f4a6ea2aed
DESKTOP-UNEFRJO\Flankao <fate45678@gmail.com>
2019-01-24 17:46:10 +0800

    登入以及註冊

--- WebPlatForm/Controllers/LoginController.cs
+++ WebPlatForm/Controllers/LoginController.cs
@@ -22,6 +22,11 @@ namespace WebPlatForm.Controllers
             return PartialView();
         }
 
+        public ActionResult NewUser()
+        {
+            return PartialView();
+        }
+
         public ActionResult LoginCheck(string Account, string Password)
         {
             //var Tpgis = new TpgisRepository();
